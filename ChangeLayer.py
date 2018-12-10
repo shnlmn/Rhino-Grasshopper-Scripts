50b0a0163456d56353ced119f2d8f8407405da14
shaleaman <magnetscoil@gmail.com>
2018-12-10 10:42:44 -0800

    removed TOOLS dir

--- ChangeLayer.py
+++ ChangeLayer.py
@@ -5,6 +5,14 @@
 # Created:     08/31/2017
 #------------------------------------------------------
 
+"""
+For use with the Feasibility Study Workflow. 
+See "F:\Resource\Strategic Initiatives\0702013.20 Research Initiative\Rhino Workflow"
+
+This script also requires getLayerList.py to be in the same folder.
+
+"""
+
 import rhinoscriptsyntax as rs
 import re
 import scriptcontext as sc
@@ -12,11 +20,12 @@ import Rhino
 import getLayerList
 
 def main():
+    sc.doc = Rhino.RhinoDoc.ActiveDoc
     rs.EnableRedraw(enable=False)
 
     topList = []
-    topList = sorted(getLayerList.getLayers(exclude=[".3dm", "Make2D"]))
-    print(topList)
+    topList = sorted(getLayerList.getLayers(exclude = ["3D","3dm"]))
+    
     thisLayer = rs.CurrentLayer()
     
     topList.append("TURN ALL ON")
@@ -31,17 +40,23 @@ def main():
     elif destinationLayer == "TURN ALL ON":
         topList.remove(destinationLayer)
         for layer in topList:
+            sc.doc = Rhino.RhinoDoc.ActiveDoc
+            
             rs.LayerVisible(layer, True)
-#            rs.ExpandLayer(layer, False)
+            rs.ExpandLayer(layer, False)
             
     else:
+        sc.doc = Rhino.RhinoDoc.ActiveDoc
+        
         topList.remove("TURN ALL ON")
         topList.remove(destinationLayer)
         rs.CurrentLayer(layer = destinationLayer)
         rs.ExpandLayer(destinationLayer, True)
         for layer in topList:
+            sc.doc = Rhino.RhinoDoc.ActiveDoc
+            
             rs.LayerVisible(layer, False)
-#            rs.ExpandLayer(layer, False)
+            rs.ExpandLayer(layer, False)
             
     print(destinationLayer)
     rs.EnableRedraw(enable=True)
